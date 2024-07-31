f214486e8d534877f6a6a20410a12b6a892621e2
komal-kumbhar <komalkumbhar9898@gmail.com>
2024-07-31 07:52:19 +0530

    Javascript variables
    Javascript variables

---
+++ 01-variable-assignment.js
@@ -0,0 +1,5 @@
+console.log("My city name is: Kolhapur");
+console.log("My best friend is: Jessy");
+console.log("My country is: India");
+console.log("My school name is: Y.P. Highschool, Solankur");
+console.log("College name is: SUK, Kolhapur ");
