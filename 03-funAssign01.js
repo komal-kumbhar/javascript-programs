a2c96ca17fe2b9cdf0b6ef168a3c2301bd4fd006
komal-kumbhar <komalkumbhar9898@gmail.com>
2024-08-02 13:59:27 +0530

    JavaScript Programs - Function Assignments
     Function Assignments :
    03-funAssign01.js
    03-funAssign02.js

---
+++ 03-funAssign01.js
@@ -0,0 +1,33 @@
+console.log("==== Function with No Argument and No Return====");
+
+
+function student() {
+    console.log("I am learning JavaScript language") 
+}
+student();
+
+function work() {
+    console.log("Working with team");
+    
+}
+work();
+
+console.log("=========Personal Details=========");
+
+function personalDetails(firstName, lastName, collegeName){
+    console.log("First Name is:",firstName)
+    console.log("Last Name is:",lastName)
+    console.log("College Name is:",collegeName)
+    
+}
+personalDetails("Komal", "Kumbhar", "YP college");
+
+console.log("=======Addition of Values");
+function addThreeValues(val1, val2, val3) {
+    var result= val1 + val2 + val3
+    console.log("Addition is :",result);
+}
+addThreeValues(10.23, 600, 40);
+addThreeValues("Hello", "Good", "Morning");
+
+
---
+++ 03-funAssign02.js
@@ -0,0 +1,15 @@
+console.log("========Normal Function =========");
+function bankDetails(bankName, accountNum, location, pinCode){
+    console.log("Bank Name : ",bankName)
+    console.log("Account Number : ",accountNum)
+    console.log("Location : ",location)
+    console.log("Pin Code : ",pinCode)   
+}
+console.log("=========Branch 1 ===========");
+bankDetails("CITI Bank", 3456782345, 'Pune', 431202);
+
+console.log("=========Branch 2 ===========");
+bankDetails("Axis Bank", 7856782345, 'Mumbai',441202);
+
+console.log("=========Branch 3 ===========");
+bankDetails('HDFC Bank',8956782345,'Pune',631202, "Open");
