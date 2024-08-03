4e996f3de99c80b8f10b632164c45ce45e5e151f
komal-kumbhar <komalkumbhar9898@gmail.com>
2024-08-03 13:43:40 +0530

    Basic string assignment -using String Template
    Basic string assignment - 03-stringBasic01.js

---
+++ 03-stringBasicAssign01.js
@@ -0,0 +1,26 @@
+
+console.log(`======== No Argument No Return =====`);
+function stringBasic(){
+    console.log(`My dream company is "TSYS"`);  
+}
+stringBasic();
+
+console.log(`======== Hobbies =======`);
+var hobbyOne = "Reading";
+var hobbyTwo = "Sketching";
+var hobbyThree = "Traveling";
+console.log(`My hobbies are : ${hobbyOne}, ${hobbyTwo}, ${hobbyThree}`);
+
+console.log(`======== Sum of hobbies characters =======`);
+
+var hobbyOneLength = hobbyOne.length;
+// console.log(hobbyOneLength);
+var hobbyTwoLength = hobbyTwo.length;
+// console.log(hobbyTwoLength);
+var hobbyThreeLength = hobbyThree.length;
+// console.log(hobbyThreeLength);
+
+console.log(`Total number of characters are :${hobbyOneLength+ hobbyTwoLength +hobbyThreeLength}`);
+
+
+
