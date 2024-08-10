10a05179b074eb020e7af7a6372c90555a8d0b60
komal-kumbhar <komalkumbhar9898@gmail.com>
2024-08-10 17:07:25 +0530

    Control flow -For Loop
    06-for-loop.js
    Assignment :
    06-for-loop-assignment.js

---
+++ 06-for-loop-assignment01.js
@@ -0,0 +1,39 @@
+console.log(`Numbers from 5 to 15`);
+
+for(let i=5; i<=15; i++){
+    console.log(i);
+    
+}
+
+console.log(`Numbers from 50 to 40`);
+for(let i=50; i>=40; i--){
+    console.log(i);
+    
+}
+
+console.log(`15 odd numbers`);
+for (let index = 1; index <=30; index=index+2) {
+    console.log(index);    
+}
+
+console.log(`10 Even numbers`);
+for (let index = 0; index <=20; index=index+2) {
+   console.log(index);
+      
+}
+console.log(`Table of 5`);
+
+for(let i=5; i<=50; i=i+5) {  
+    console.log(i);  
+}
+
+console.log(`Table of 10`);
+for(i=1; i<=10; i++){
+    var x = 10 * i;
+    console.log(x);   
+}
+
+console.log(`Reverse table of 10`);
+for(i=100; i>=10; i= i-10){
+    console.log(i);   
+}
